62f0e743d76beb85ddf8604d3d72ff629e8b804a
Pokreni se web <pokreniseweb@Pokrenis-MacBook-Pro.local>
2019-10-18 01:10:15 +0200

    creating store and installing axios

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vuex from 'vuex';
+import Vue from 'vue';
+import movies from './modules/movies';
 
+// Load Vuex
 Vue.use(Vuex);
 
+// Create store
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  modules: {
+    movies
+  }
 });
