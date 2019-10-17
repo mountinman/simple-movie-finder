f3d899e4058fd29064cc21002e31cd02b235192e
Pokreni se web <pokreniseweb@Pokrenis-MacBook-Pro.local>
2019-10-17 22:52:27 +0200

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Movie</span>
+        <span class="font-weight-light">FINDER</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text>
+        <span class="mr-2">FIND MOVIE</span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content></v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {};
   }
-}
+};
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
