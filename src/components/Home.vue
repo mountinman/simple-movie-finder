517f87ae1d206804527480a90f19b242476f6f8b
Pokreni se web <pokreniseweb@Pokrenis-MacBook-Pro.local>
2019-10-18 09:47:57 +0200

    Adding carousel of most popular movies

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="main-home-container">
-    <div v-for="movie in allMovies" :key="movie.title">
-      <p>{{movie.title}}</p>
-    </div>
-    <div v-for="popular in popularMovies" :key="popular.title">
-      <p>{{popular.title}}</p>
-    </div>
+  <div class="main-home-container d-flex pa-2">
+   <v-carousel
+    cycle
+    height="400"
+   
+    show-arrows-on-hover
+   >
+     <v-carousel-item
+     v-for="movie in popularMovies"
+     :key="movie.title"
+     :src="movie.imageUrl"
+     >
+     <div class="carousel-title">
+       <p>{{movie.title}}</p>
+     </div>
+     </v-carousel-item>
+   </v-carousel>
     <v-btn id="floating-btn" to="/find-movie" text>
       <span class>FIND</span>
     </v-btn>
@@ -29,5 +39,14 @@ export default {
   right: 50px;
   bottom: 50px;
   background: $btn-color;
+}
+.carousel-title{
+  color:white;
+  position: relative;
+  top: 350px;
+  font-size: 30px;
+  margin-left: 20px;
+  font-weight: 700;
+  text-transform: uppercase;
 }
 </style>
--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,7 +1,16 @@
 
 const state = {
   allMovies: [{ title: "movie1" }, { title: "movie2" }],
-  popularMovies: [{ title: "pop1" }, { title: "pop2" }]
+  popularMovies: [
+    { 
+      title: "pop1",
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg'
+    }, 
+    { 
+      title: "pop2",
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg'
+    }
+  ]
 };
 
 const getters = {
