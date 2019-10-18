9c415c02097276e5ec5ab071f4690f637b0c9869
Pokreni se web <pokreniseweb@Pokrenis-MacBook-Pro.local>
2019-10-18 11:58:40 +0200

    binding dummy data to all movies component

--- src/components/AllMovies.vue
+++ src/components/AllMovies.vue
@@ -6,7 +6,42 @@
       v-for="movie in allMovies"
       :key="movie.id"
       cols="12" md="6">
-        <v-card class="pa-2" outlined tile>{{movie.title}}</v-card>
+        <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      :src="movie.imageUrl"
+    >
+      <v-card-title>{{movie.title}}</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">POPULARITY</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>DESCRIPTION</div>
+
+      
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="orange"
+        text
+      >
+        Share
+      </v-btn>
+
+      <v-btn
+        color="orange"
+        text
+      >
+        Explore
+      </v-btn>
+    </v-card-actions>
+  </v-card>
       </v-col>
       
     </v-row>
--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,6 +1,27 @@
 
 const state = {
-  allMovies: [{ title: "movie1" }, { title: "movie2" }],
+  allMovies: [
+    { 
+      id:'4983rp',
+      title: "pop1",
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg'
+    }, 
+    { 
+      id:'hjf9h3',
+      title: "pop2",
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg'
+    },
+    { 
+      id:'4783rp',
+      title: "pop1",
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg'
+    }, 
+    { 
+      id:'hjD9h3',
+      title: "pop2",
+      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/47/New_york_times_square-terabass.jpg'
+    }
+  ],
   popularMovies: [
     { 
       id:'4983rp',
