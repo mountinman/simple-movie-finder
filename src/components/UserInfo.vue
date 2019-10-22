2a785b66272370218fe198dfe79f04532032963f
Marko Cvrljak <marko.cvrljak1@gmail.com>
2019-10-22 16:21:34 +0200

    Update UserInfo.vue

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,13 +1,43 @@
 <template>
-  <p>user</p>
+  <div>
+    <div 
+    class="movie-rating"
+    v-for="movie in popularMovies" 
+    :key="movie.id">
+      <h4>{{movie.title}}</h4>
+      <star-rating @rating-selected="updateMovie" :max-rating="10" v-model="movie.rating"></star-rating>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import StarRating from "vue-star-rating";
 
-}
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    StarRating
+  },
+  computed: {
+    popularMovies() {
+      return this.$store.getters.getPopularMovies.filter(movie => {
+        if (movie.rating) {
+          return movie;
+        }
+      });
+    }
+  }
+};
 </script>
 
-<style>
 
+<style>
+.movie-rating{
+  padding: 20px;
+  border: 1px solid gray;
+  width: 700px;
+  margin: 20px auto;
+}
 </style>
